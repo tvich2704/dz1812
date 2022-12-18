529b3ccd89ea370aa0b97b0e7831b62f4143df28
krasnobaev.a.a <tvich2704@gmail.com>
2022-12-18 14:08:37 +0500

    zadach2

---
+++ homwork2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите два числа a и b");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine($"a={a};b={b}->max={a}");
+}
+else
+{
+    if (a == b)
+    {
+        Console.WriteLine($"a=b={a}");
+    }
+    else
+    {
+        Console.WriteLine($"a={a};b={b}->max={b}");
+    }
+}
+
+
