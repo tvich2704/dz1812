45900810bc1564ae2234a6fb24ce624a1f273e7b
krasnobaev.a.a <tvich2704@gmail.com>
2022-12-18 15:15:10 +0500

    zadach8

---
+++ homework8/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = int.Parse(Console.ReadLine());
+int count = 1;
+Console.Write($"{n}->");
+while (count <= n)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write($"{count} ");
+    }
+    count += 1;
+}
+
