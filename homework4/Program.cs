4b5f7dd2da66ce93ae9b3c2a31b373aa105de4cb
krasnobaev.a.a <tvich2704@gmail.com>
2022-12-18 15:11:30 +0500

    zadach4

---
+++ homework4/Program.cs
@@ -0,0 +1,52 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+if (a > b & a > c)
+{
+    Console.WriteLine($"{a},{b},{c}->{a}");
+}
+if (b > a & b > c)
+{
+    Console.WriteLine($"{a},{b},{c}->{b}");
+}
+if (c > b & c > a)
+{
+    Console.WriteLine($"{a},{b},{c}->{c}");
+}
+else
+{
+    if (a == b & a == c) // Если все равны
+    {
+    Console.WriteLine($"{a},{b},{c}->равны");
+    }
+    else // Если равны два числа
+    {
+        if (a == b & a > c)
+        {
+            Console.WriteLine($"{a},{b},{c}->{a}");
+        }
+        else
+        {
+            Console.WriteLine($"{a},{b},{c}->{c}");
+        }
+        if (a == c & a > b)
+        {
+            Console.WriteLine($"{a},{b},{c}->{a}");
+        }
+        else
+        {
+            Console.WriteLine($"{a},{b},{c}->{b}");
+        }
+        if (c == b & c > a)
+        {
+            Console.WriteLine($"{a},{b},{c}->{c}");
+        }
+        else
+        {
+            Console.WriteLine($"{a},{b},{c}->{a}");
+        }
+    }
+}
+
+
